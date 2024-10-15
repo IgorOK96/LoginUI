0641d036e17b6662ccbdb533cb853793388b2310
IgorOK96 <IgorOK96@users.noreply.github.com>
2024-10-15 18:00:39 +0200

    Initial Commit

---
+++ LoginUI/LoginUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginUIApp.swift
+//  LoginUI
+//
+//  Created by user246073 on 10/15/24.
+//
+
+import SwiftUI
+
+@main
+struct LoginUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
